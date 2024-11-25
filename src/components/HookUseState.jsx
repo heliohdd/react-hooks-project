8798b43011ad46777d2148dc63433b102ffd8208
Helio Dourado <heliohdd@yahoo.com.br>
2024-11-25 13:42:50 -0300

    Hook useState para inputs em formulario

--- src/components/HookUseState.jsx
+++ src/components/HookUseState.jsx
@@ -1,19 +1,27 @@
 import { useState } from "react";
 
 const HookUseState = () => {
-    let userName = "Hélio"
+  let userName = "Hélio";
 
-    const [name, setName] = useState("Érica")
+  const [name, setName] = useState("Érica");
 
-    const changeNames = () => {
-        userName = "Hélio Dourado"
+  const changeNames = () => {
+    userName = "Hélio Dourado";
 
-        setName("Érica Ori")
+    setName("Érica Ori");
 
-        console.log(userName)
-        console.log(name)
-    }
-    
+    console.log(userName);
+    console.log(name);
+  };
+
+  const [age, setAge] = useState(18);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(age)
+  };
+
   return (
     <div>
       <hr />
@@ -21,6 +29,18 @@ const HookUseState = () => {
       <p>Valor da variável userName: {userName}</p>
       <p>Valor do state name: {name}</p>
       <button onClick={changeNames}>Clicar</button>
+      <hr />
+      <form onSubmit={handleSubmit}>
+        <h2>Hook useState para inputs em campo de formulário</h2>
+        <p>Digite sua idade:</p>
+        <input
+          type="text"
+          onChange={(e) => setAge(e.target.value)}
+          value={age}
+        />
+        <input type="submit" value="Atualizar" />
+      </form>
+      <p>Você tem {age} anos</p>
     </div>
   );
 };
