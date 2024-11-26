8d636275868a2a83bc68859e75fbf14b59c691da
Helio Dourado <heliohdd@yahoo.com.br>
2024-11-26 13:31:15 -0300

    Cleanup no useEffect

--- src/components/HookUseEffect.jsx
+++ src/components/HookUseEffect.jsx
@@ -19,12 +19,37 @@ const HookUseEffect = () => {
     console.log("Serei executado apenas uma vez!");
   }, []);
 
+  // Caso de Uso do hook useEffect com item/itens na dependência
+  // é executado quando algum item é alterado
+  const [anotherNumber, setAnotherNumber] = useState(0);
+  useEffect(() => {
+    if (anotherNumber) {
+      console.log("Sou executado apenas quando o anotherNumber muda!");
+    }
+  }, [anotherNumber]);
+
+  // Cleanup no useEffect
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log("Hello world!!!");
+
+      // setAnotherNumber(anotherNumber + 1);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [anotherNumber]);
+
   return (
     <div>
       <hr />
       <h1>Caso de Uso do hook useEffect sem dependências</h1>
       <p>Number: {number}</p>
       <button onClick={changeSomething}>Executar!</button>
+      <hr />
+      <h2>Caso de Uso do hook useEffect com itens na dependência</h2>
+      <button onClick={() => setAnotherNumber(anotherNumber + 1)}>
+        Use Effect
+      </button>
     </div>
   );
 };
